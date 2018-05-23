897fdb1e5d37bcaf68fce9a5a049e543fef8b41b
Martin Saulis <martin@saulis.com>
2018-05-23 17:22:26 +0300

    update tests

--- src/validators/index.test.js
+++ src/validators/index.test.js
@@ -1,18 +1,18 @@
 import { required } from './';
 
 test('required passing', () => {
-  expect(required('hello')).toBe(undefined);
+  expect(required()('hello')).toBe(undefined);
 });
 
 test('required failing', () => {
-  expect(required('')).toBe('Required');
+  expect(required()('')).toBe('Required');
 });
 
 test('required array passing', () => {
-  expect(required([1])).toBe(undefined);
+  expect(required()([1])).toBe(undefined);
 });
 
 test('required array failing', () => {
-  expect(required([])).toBe('Required');
+  expect(required()([])).toBe('Required');
 });
 
